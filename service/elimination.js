6407f39ccc9e227ece3cebf728b48f0fd8e20343
Boniphace <boniphacebageme06@gmail.com>
2025-12-23 01:40:38 +0300

    i added the simple logic to find the leading ones

--- service/elimination.js
+++ service/elimination.js
@@ -1,14 +1,28 @@
 export function solveGaussian(matrix){
     const steps = matrix;
+   
+    
+    for(let i=0; i<3; i++){
+        if(steps[0][0]==="1"){
+            console.log(steps[0]);
+        }
+        break;
+    }
 
-    steps.forEach(row => {
-        row.forEach((value,index)=>{
-            console.log(typeof(value))
-        })
+
+    // steps.forEach(row => {
+    //     row.forEach((value,index)=>{
+    //         console.log(typeof(value))
+    //     })
         
-    });
+    // });
 
-    console.log(steps)
+    // steps.forEach((value,index)=>{
+            
+    //         console.log(value);
+    //     //console.log(value[index]); 
 
+    // });
+    
     return steps;
 }
